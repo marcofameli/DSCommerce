cf538d78d6ba66d70d242505e30fbcb445795aec
marquola <zordnew@gmail.com>
2024-11-05 22:04:19 -0300

    Bean Validation adicionado

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -3,13 +3,21 @@ package com.devsuperior.dscommerce.dto;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(min = 3, max = 80, message = "Deve ter entre 3 e 80 caracteres")
+    @NotBlank(message = "Campo obrigatório")
     private String name;
+    @Size(min = 10, message = "Deve ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo obrigatório")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
